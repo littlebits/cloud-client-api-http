ee1750aa203757ea732d0cfefb12cbe46eb085da
Jason Kuhrt <jasonkuhrt@me.com>
2014-05-30 15:04:05 -0400

    Use process stdout in download script instead of fs module

--- scripts/download-routes.js
+++ scripts/download-routes.js
@@ -1,14 +1,12 @@
-'use strict';
 var r = require('request');
-var fs = require('fs');
 
 
 var uri = 'https://api-rest-stage.littlebitscloud.cc/options';
 
-function save_data(err, res, body){
+function handle_response(err, res, body){
   var contents = 'module.exports = ' + JSON.stringify(body.routes, null, 2) +';';
-  fs.writeFileSync((__dirname + '/../assets/routes.js'), contents);
+  process.stdout.write(contents);
 }
 
 
-r.get(uri, {json: true}, save_data);
+r.get(uri, {json: true}, handle_response);
