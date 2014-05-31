54eb306c3781d2a4b79a4afe4a7512b6f7ca5105
Jason Kuhrt <jasonkuhrt@me.com>
2014-05-30 21:41:58 -0400

    ocd

--- scripts/download-routes.js
+++ scripts/download-routes.js
@@ -4,10 +4,14 @@ var r = require('request');
 // var uri = 'https://api-rest-stage.littlebitscloud.cc/options';
 var uri = 'http://localhost:8000/options';
 
+r.get(uri, {json: true}, handle_response);
+
+
+
 function handle_response(err, res, body){
-  var contents = 'module.exports = ' + JSON.stringify(body, null, 2) +';';
-  process.stdout.write(contents);
+  process.stdout.write(prepare_source(body));
 }
 
-
-r.get(uri, {json: true}, handle_response);
+function prepare_source(source){
+  return 'module.exports = ' + JSON.stringify(source, null, 2) + ';';
+}
